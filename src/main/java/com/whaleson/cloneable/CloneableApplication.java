e1ca253d255bb6730948cfb8a106cea178eaaabe
WhalesonLiu <1135242369@qq.com>
2019-09-14 17:08:07 +0800

    initialize

---
+++ src/main/java/com/whaleson/cloneable/CloneableApplication.java
@@ -0,0 +1,13 @@
+package com.whaleson.cloneable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whaleson/cloneable/CloneableApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whaleson.cloneable;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloneableApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
